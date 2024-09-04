b3f19c5a26e39ee0437abcb53fbb253315495020
Marina <marina.p@carbonclick.com>
2024-09-04 21:20:52 +0300

    Toolbar

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,42 @@
-const DocumentIdPage: React.FC = () => {
+'use client';
+
+import { useQuery } from 'convex/react';
+import { Id } from '@/convex/_generated/dataModel';
+import { api } from '@/convex/_generated/api';
+
+import { Toolbar } from '@/components/toolbar';
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<'documents'>;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  if (document === null) {
+    return (
+      <div>Document not found</div>
+    );
+  }
+
   return (
-    <div>
-      DocumentIdPage
+    <div className='pb-40'>
+      <div className='h-[35vh]' />
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar initialData={document} />
+      </div>
     </div>
-  )
+  );
 };
 
-export default DocumentIdPage
+export default DocumentIdPage;
