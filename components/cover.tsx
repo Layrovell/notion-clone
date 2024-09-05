d5d941c9e9479a47a2c00effc9e09a5381700733
Marina <marina.p@carbonclick.com>
2024-09-05 16:48:02 +0300

    Advanced file upload

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import { Button } from './ui/button';
-import { ImageIcon, X } from 'lucide-react';
-import { useCoverImage } from '@/hooks/use-cover-image';
-import { api } from '@/convex/_generated/api';
-import { useMutation } from 'convex/react';
 import { useParams } from 'next/navigation';
+import { ImageIcon, X } from 'lucide-react';
+
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
+
+import { useCoverImage } from '@/hooks/use-cover-image';
+import { Button } from './ui/button';
+
+import { useEdgeStore } from '@/lib/edgestore';
+import { cn } from '@/lib/utils';
 
 interface CoverImageProps {
   url?: string;
@@ -16,11 +20,18 @@ interface CoverImageProps {
 }
 
 export const Cover = ({ url, preview }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
+
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<'documents'>,
     });
@@ -32,12 +43,22 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
 
       {url && !preview && (
         <div className='opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2'>
-          <Button onClick={coverImage.onOpen} variant='outline' size='sm' className='text-muted-foreground text-xs'>
+          <Button
+            onClick={() => coverImage.onReplace(url)}
+            variant='outline'
+            size='sm'
+            className='text-muted-foreground text-xs'
+          >
             <ImageIcon className='h-4 w-4 mr-2' />
             Edit
           </Button>
 
-          <Button onClick={onRemove} variant='outline' size='sm' className='text-muted-foreground text-xs'>
+          <Button
+            onClick={onRemove}
+            variant='outline'
+            size='sm'
+            className='text-muted-foreground text-xs'
+          >
             <X className='h-4 w-4 mr-2' />
             Remove
           </Button>
